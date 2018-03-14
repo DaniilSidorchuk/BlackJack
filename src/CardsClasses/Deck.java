0826abb9588760ea804336137f26229a43ed42e0
DaniilSidorchuk <daniilsidorchuk@gmail.com>
2018-03-14 20:25:56 +0200

    4 edition

--- src/CardsClasses/Deck.java
+++ src/CardsClasses/Deck.java
@@ -27,8 +27,8 @@ public class Deck {
         Suit [] suit = new Suit[]{Suit.Clubs,Suit.Diamonds,Suit.Hearts,Suit.Spades};
         Rang [] rangs = new Rang[]{Rang.ACE, Rang.KING,Rang.QEEN,Rang.JACK,Rang.TEN,Rang.NINE,Rang.EIGHT,Rang.SEVEN,Rang.SIX,Rang.FIVE,Rang.FOUR,Rang.THREE,Rang.TWO};
 
-        int suitRandomIndex = (int) Math.random()*3;
-        int rangsRandomIndex = (int) Math.random()*12;
+        int suitRandomIndex = (int) (Math.random() * 3);
+        int rangsRandomIndex = (int) (Math.random() * 12);
 
         return new Card(suit[suitRandomIndex],rangs[rangsRandomIndex]);
 
@@ -39,11 +39,11 @@ public class Deck {
     }
 
     public void shuffleTheDeck(){
-        int randomNumber = (int) Math.random()*25+10;
+        int randomNumber = (int) (Math.random() * 25) +10;
 
         for (int i = 0; i < randomNumber; i++) {
-            int index1 = (int) Math.random()*deck.size();
-            int index2 = (int) Math.random()*deck.size();
+            int index1 = (int) (Math.random() * deck.size());
+            int index2 = (int) (Math.random() * deck.size());
            Card card = deck.get(index1);
            deck.set(index1, deck.get(index2));
            deck.set(index2,card);
